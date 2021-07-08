b366dbb80e25bbe40c7f80a88b128e7b8a3783ca
NakshatraCodes <nakshatra163@gmail.com>
2021-07-08 21:55:09 +0530

    readme service

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+const getURL = (username, repo, branch = 'master') => {
+  return `https://raw.githubusercontent.com/${username}/${repo}/${branch}/README.md`;
+};
 
 app.use(express.json());
 
@@ -8,6 +13,23 @@ app.get('/', (req, res) => {
   res.json({
     success: true,
     message: 'OK',
+  });
+});
+
+app.post('/readme', async (req, res) => {
+  const { username, repository, branch } = req.body;
+  if (!username || !repository) {
+    return res.json({
+      success: false,
+      message: 'Username or Repo not defined',
+    });
+  }
+  let response = await axios.get(getURL(username, repository, branch));
+
+  res.json({
+    success: true,
+    message: 'OK',
+    data: response.data,
   });
 });
 
