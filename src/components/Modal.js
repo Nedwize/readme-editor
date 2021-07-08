aabed5c0a256091a487f635912ab1e7cc7a28c27
NakshatraCodes <nakshatra163@gmail.com>
2021-07-08 20:55:24 +0530

    ui changes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import { Modal, Button, Form, Col, Row } from 'react-bootstrap';
 
 const ModalComponent = ({ show, handleClose }) => {
   return (
@@ -13,18 +13,31 @@ const ModalComponent = ({ show, handleClose }) => {
             <Form.Label>Username or Organization</Form.Label>
             <Form.Control type="text" placeholder="Enter Username" />
           </Form.Group>
-          <Form.Group controlId="formGroupPassword">
-            <Form.Label>Repository</Form.Label>
-            <Form.Control type="text" placeholder="Repository" />
-          </Form.Group>
-          <Form.Group controlId="formGroupPassword">
-            <Form.Label>Branch (default main)</Form.Label>
-            <Form.Control type="text" placeholder="Branch" />
-          </Form.Group>
+          <Row>
+            <Col>
+              <Form.Group controlId="formGroupPassword">
+                <Form.Label>Repository</Form.Label>
+                <Form.Control type="text" placeholder="Repository" />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="formGroupPassword">
+                <Form.Label>Branch (default main)</Form.Label>
+                <Form.Control type="text" placeholder="Branch" />
+              </Form.Group>
+            </Col>
+          </Row>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="primary" onClick={handleClose}>
+        <Button
+          variant="primary"
+          style={{
+            backgroundColor: '#007bff',
+            color: 'white',
+          }}
+          onClick={handleClose}
+        >
           Find
         </Button>
       </Modal.Footer>
